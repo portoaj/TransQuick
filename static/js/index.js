cc96ca5f901e3562cbae7c340a1f594853607f3e
Andrew Masek <portoaj@users.noreply.github.com>
2022-04-09 13:47:09 -0700

    Request working

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,16 @@
 function summarize(){
   console.log('test');
   const textBoxValue = document.getElementById("transcript").value;
-  fetch("/",{
-    method: "POST",
-    headers: {"Content-Type": "text/plain"},
-    body: textBoxValue
-  }).then(res => {
+
+  const options = {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(textBoxValue),
+    };
+
+  fetch("/", options).catch(err => console.log(err)).then(res => {
     console.log(res);
   });
 }
